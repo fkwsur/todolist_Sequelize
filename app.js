e8cf2af15a70f211879da91bedb3387f9ccdfc1f
fkwsurm <guswl543210@gmail.com>
2021-06-15 22:53:29 +0900

    수정

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const Router = require('./routes');
-const model = require('./models')
 const db = require('./models')   // mysql 시퀄라이저 모델
-var http = require('http');
+const http = require('http');
 http.createServer(app).listen(8080, () => {
   console.log('server on');
 });
@@ -12,12 +11,17 @@ http.createServer(app).listen(8080, () => {
 db.sequelize
 .authenticate()
 .then(async () => {
+  try{
     console.log('db connect ok');
     await db.sequelize.sync({force : false});
+  }catch(err){
+    console.log('err');
+  }  
 })
 .catch(err => {
     console.log('db' + err);
 });
+
 
 app.use(cors());
 app.use(express.json())
